a59695835aa83ed32173f50d4b5e87e75a71b484
nonaka <info@kantan-pro.com>
2023-01-21 12:16:52 +0900

    ページャー

--- includes/class-tab-client.php
+++ includes/class-tab-client.php
@@ -11,22 +11,20 @@ class Kntan_Client_Class{
         // add_filter('');
     }
     
-    // クライアントテーブル作成
-    function Client_Table_Create(){
+    // -----------------------------
+    // テーブル作成
+    // -----------------------------
+
+    function Create_Table( $name ){
         
-        // テーブルのバージョン管理
         global $wpdb;
-        global $my_client_table_version;
-        $my_client_table_version = '1.0.1'; // 更新する場合はバージョンを変更
-
-        // テーブル名を設定
-        $table_name = $wpdb->prefix. 'ktpwp_client';
-
-        // 文字コードを設定
-        $charset_collate = $wpdb->get_charset_collate();
+        global $my_table_version;
+        $my_table_version = '1.0.1'; // 更新する場合はバージョンを変更
+        $table_name = $wpdb->prefix . 'ktp_' . $name; // テーブル名を設定
+        $charset_collate = $wpdb->get_charset_collate(); // 文字コードを設定
 
         // テーブル名またはテーブルバージョンを変更した場合にdbDelta($sql)が実行される
-        if ($wpdb->get_var("show tables like '$table_name'") != $table_name || get_option('my_client_table_version') !== $my_client_table_version) {
+        if ($wpdb->get_var("show tables like '$table_name'") != $table_name || get_option('my_table_version') !== $my_table_version) {
             $sql = $wpdb->prepare("CREATE TABLE " . $table_name . " (
                 id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
                 time BIGINT(11) DEFAULT '0' NOT NULL,
@@ -37,49 +35,38 @@ class Kntan_Client_Class{
             ) $charset_collate;");
 
             require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
-            // テーブル更新実行
-            dbDelta($sql);
-            
-            // wp_optionsにテーブルバージョンを登録する
-            add_option( 'my_client_table_version', $my_client_table_version );
-
-            // wp_optionsにテーブルバージョンをアップデートする
-            update_option( 'my_client_table_version', $my_client_table_version );
+            dbDelta($sql); // テーブル作成実行
+            add_option( 'ktp_'.$name.'_table_version', $my_table_version ); // wp_optionsにテーブルバージョンを登録する 
+            update_option( 'ktp_'.$name.'_table_version', $my_table_version ); // wp_optionsにテーブルバージョンをアップデートする
         }
     
     }
-    
-    // フォームから値を受信しレコードを更新または追加する
-    function Client_Table_Data() {
+
+    // -----------------------------
+    // テーブルの操作（更新・追加・削除）
+    // -----------------------------
+
+    function Update_Table( $name ) {
+
         global $wpdb;
-        $table_name = $wpdb->prefix. 'ktpwp_client';
+        $table_name = $wpdb->prefix . 'ktp_' . $name;
         
         // POSTデーター受信
-        $client_id = $_POST['client_id'];
+        $data_id = $_POST['data_id'];
         $query_post = $_POST['query_post'];
-        $client_name = $_POST['client_name'];
+        $data_name = $_POST['data_name'];
         $text = $_POST['text'];
-        
-        // // GETデーター受信
-        // $client_id = $_GET['client_id'];
-        // $query_post = $_GET['query_post'];
-        // $client_name = $_GET['client_name'];
-        // $text = $_GET['text'];
-        
+                
         // 更新
         if( $query_post == 'update' ){
             
-            // // クッキーでクライアントIDを取得
-            // $client_id = $_COOKIE['ktp_client_id'];
-
             $wpdb->update( 
                 $table_name, 
                 array( 
-                    'name' => $client_name,
+                    'name' => $data_name,
                     'text' => $text,
                 ),
-                array( 'ID' => $client_id ), 
+                array( 'ID' => $data_id ), 
                 array( 
                     '%s',	// name
                     '%s'	// text
@@ -96,7 +83,7 @@ class Kntan_Client_Class{
                 $table_name, 
                 array( 
                     'time' => current_time( 'mysql' ),
-                    'name' => $client_name,
+                    'name' => $data_name,
                     'text' => $text,
                 ) 
             );
@@ -108,7 +95,7 @@ class Kntan_Client_Class{
             $wpdb->delete(
                 $table_name,
                 array(
-                    'id' => $client_id
+                    'id' => $data_id
                 ),
                 array(
                     '%d'
@@ -124,8 +111,15 @@ class Kntan_Client_Class{
 
     }
 
-    // 表示する
-    function Client_Table_View( $name ) {
+    // -----------------------------
+    // テーブルの表示
+    // -----------------------------
+
+    function View_Table( $name ) {
+
+        // echo $name;
+        // exit;
+
 
         global $wpdb;
 
@@ -135,74 +129,143 @@ class Kntan_Client_Class{
 
         // ログアウトのリンク
         $logout_link = wp_logout_url();
-        
-        // テーブルデータを表示する（ページャー）
-        $query_limit = '20'; //表示範囲
-        $query_num = '0'; //スタート位置
-        $query_range = $query_num . ',' . $query_limit;
-        $table_name = $wpdb->prefix . 'ktpwp_client';
 
-        // クライアントリスト表示
+        // リストヘッダ表示
+        $results_h = <<<END
+        <div class="data_contents">
+        <div class="data_list_box">
+        <h3>■ 顧客リスト</h3>
+        END;
+        
+        //
+        // リスト表示
+        //
+
+        $table_name = $wpdb->prefix . 'ktp_' . $name;
+
+        //表示範囲
+        $query_limit = '6';
+
+        //スタート位置を決める
+        $page_stage = $_GET['page_stage'];
+        $page_start = $_GET['page_start'];
+        
+        if( $page_stage == '' ){
+            $page_start = 0;
+        }
+
+        $query_range = $page_start . ',' . $query_limit;
         $query = $wpdb->prepare("SELECT * FROM {$table_name} ORDER BY `id` ASC LIMIT $query_range");
         $post_row = $wpdb->get_results($query);
-        $results_h = <<<END
-        <div class="client_contents">
-            <div class="client_list_box">
-            <h3>■ 顧客リスト($query_range)</h3>
-        END;
-        foreach ($post_row as $row){
-            $id = esc_html($row->id);
-            $time = esc_html($row->time);
-            $client_name = esc_html($row->name);
-            $text = esc_html($row->text);
+        if( $post_row ){
+            foreach ($post_row as $row){
+                $id = esc_html($row->id);
+                $time = esc_html($row->time);
+                $data_name = esc_html($row->name);
+                $text = esc_html($row->text);
+                $results[] = <<<END
+                <div class="data_list_item">$id : $time : $data_name : $text : <a href="?tab_name=$name&data_id=$id&page_start=$page_start&page_stage=$page_stage"> → </a></div>
+                END;
+            }
+            $query_max_num = $wpdb->num_rows;
+            // $post_num = count($post_row);
+        } else {
             $results[] = <<<END
-            <div class="client_list_item">$id : $time : $client_name : $text : <a href="?tab_name=$name&client_id=$id"> → </a></div>
+            <div class="data_list_item">NO DATA！</div>
             END;
         }
-        $results_f = '</div>';
-        $client_list = $results_h . implode( $results ) . $results_f;
+        
+        // ページネーションリンク
+        $post_num = count($post_row); // 現在の項目数（可変）
+        $page_buck = ''; // 前のスタート位置
+        // $page_stage = '';
+        
+        if( !$page_stage ){
+            if( $post_num >= $query_limit ){ $page_stage = 2; $page_buck = $post_num - $page_start; }else{ $page_stage = 3; }
+            $page_start ++;
+            $page_next_start = $query_max_num;
+            $results_f = <<<END
+            <div class="pagination">
+            END;
+            if( $post_num >= $query_limit ){
+                $results_f .= <<<END
+                $page_start ~ $query_max_num &emsp;<a href="?tab_name=$name&page_start=$page_next_start&page_stage=$page_stage"> > </a>
+                </div>
+                END;
+            } else {
+                $results_f .= <<<END
+                &emsp; $page_start ~ $query_max_num
+                </div>
+                END;
+            }
+            $page_start = $page_start + $query_limit;
 
-        // 任意のIDからクライアント情報を取得(GET)
-        if(isset( $_GET['client_id'] )){
-            $query_id = $_GET['client_id'];
-            // function my_setcookie() {
-            //     // IDをクッキーに保存（１０年間保存します）
-            //     setcookie( 'ktp_client_id', $query_id, time() + (20 * 365 * 24 * 60 * 60), '/' );
-            //     // タブポジションをクッキーに保存（１０年間保存します）
-            //     setcookie( 'ktp_tab_position', $name, time() + (20 * 365 * 24 * 60 * 60), '/' );
-            // }
+        } elseif( $page_stage == 2 ) {
+            if( $post_num >= $query_limit ){ $page_stage = 2; $page_buck = $post_num - $page_start; }else{ $page_stage = 3; }
+            $page_next_start = $page_start + $post_num;
+            $query_max_num = $query_max_num + $page_start;
+            $page_start ++;
+            $results_f = <<<END
+            <div class="pagination">
+            <a href="#" onclick="javascript:window.history.back(-1);return false;"> < </a>
+            END;
+            if( $post_num >= $query_limit ){
+                $results_f .= <<<END
+                &emsp; $page_start ~ $query_max_num &emsp;<a href="?tab_name=$name&page_start=$page_next_start&page_stage=$page_stage"> > </a>
+                </div>
+                END;
+            } else {
+                $results_f .= <<<END
+                &emsp; 戻る
+                </div>
+                END;
+            }
+        } elseif( $page_stage == 3 ) {
+            if( $post_num >= $query_limit ){$page_stage = 2;}else{$page_stage = 3;}
+            $results_f = <<<END
+            <div class="pagination">
+            <a href="#" onclick="javascript:window.history.back(-1);return false;"> < </a>
+            </div>
+            END;
+        }
+
+        $results_f .= '</div>';
+        $data_list = $results_h . implode( $results ) . $results_f;
+
+        // 詳細表示(GET)
+        if(isset( $_GET['data_id'] )){
+            $query_id = $_GET['data_id'];
         } else {
             $query_id = $wpdb->insert_id;
         }
-
-        // 詳細表示
+        
         $query = $wpdb->prepare("SELECT * FROM {$table_name} ORDER BY `id` = $query_id");
         $post_row = $wpdb->get_results($query);
         foreach ($post_row as $row){
-            $client_id = esc_html($row->id);
+            $data_id = esc_html($row->id);
             $time = esc_html($row->time);
-            $client_name = esc_html($row->name);
+            $data_name = esc_html($row->name);
             $text = esc_html($row->text);
         }
 
         // 表題
-        $client_title = <<<END
-        <div class="client_detail">
-            <h3>■ 顧客の詳細（ID: $client_id  TIME: $time ）</h3>
+        $data_title = <<<END
+        <div class="data_detail">
+            <h3>■ 顧客の詳細（ID: $data_id  TIME: $time ）</h3>
         END;
 
         // フォーム表示
-        $client_forms = <<<END
+        $data_forms = <<<END
                 <div class="box">
                     <form method="post" action="">
-                    <p><label> 名　　前：</label> <input type="text" name="client_name" value="$client_name"></p>
+                    <p><label> 名　　前：</label> <input type="text" name="data_name" value="$data_name"></p>
                     <p><label> テキスト：</label> <input type="text" name="text" value="$text"></p>
                     <input type="hidden" name="query_post" value="update">
-                    <input type="hidden" name="client_id" value="$client_id">
+                    <input type="hidden" name="data_id" value="$data_id">
                     <div class="submit_button"><input type="submit" name="send_post" value="更新"></div>
                     </form>
                     <form method="post" action="">
-                    <input type="hidden" name="client_id" value="$client_id">
+                    <input type="hidden" name="data_id" value="$data_id">
                     <input type="hidden" name="query_post" value="delete">
                     <div class="submit_button"><input type="submit" name="send_post" value="削除"></div>
                     </form>
@@ -210,7 +273,7 @@ class Kntan_Client_Class{
                 <div class="box">
                     <h3>■ 顧客追加</h3>
                     <form method="post" action="">
-                    <p><label> 名　　前：</label> <input type="text" name="client_name" value=""></p>
+                    <p><label> 名　　前：</label> <input type="text" name="data_name" value=""></p>
                     <p><label> テキスト：</label> <input type="text" name="text" value=""></p>
                     <input type="hidden" name="query_post" value="insert">
                     <div class="submit_button"><input type="submit" name="send_post" value="追加"></div>
@@ -225,7 +288,7 @@ class Kntan_Client_Class{
         END;
 
         // 表示するもの
-        $content = $client_list . $client_title . $client_forms . $div_end;
+        $content = $data_list . $data_title . $data_forms . $div_end;
         return $content;
 
         // POSTデータをクリア
