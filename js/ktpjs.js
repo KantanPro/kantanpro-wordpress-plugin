bc87843b71aebbe7e1a1896234fd8aa65a4d4289
nonaka <info@kantan-pro.com>
2023-12-21 20:48:26 +0900

    ask:２回目の仕事リストタブ問題

--- js/ktpjs.js
+++ js/ktpjs.js
@@ -4,13 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     tabs.forEach(function(tab) {
         tab.addEventListener('click', function() {
-            // すべてのタブとコンテンツを非アクティブに
             tabs.forEach(t => t.classList.remove('active'));
             contents.forEach(c => c.classList.remove('active'));
 
-            // クリックされたタブと対応するコンテンツをアクティブに
             tab.classList.add('active');
-            var activeContent = document.querySelector(tab.getAttribute('data-target'));
+            var activeContent = document.querySelector('#content-' + tab.id.split('-')[1]);
             if (activeContent) {
                 activeContent.classList.add('active');
             }
