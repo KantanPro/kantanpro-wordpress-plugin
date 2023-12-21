5a57664d5b49749f3c6d67951518c622e2d7d343
nonaka <info@kantan-pro.com>
2023-12-21 20:56:05 +0900

    ask:コンテンツ問題

--- js/ktpjs.js
+++ js/ktpjs.js
@@ -4,11 +4,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
     tabs.forEach(function(tab) {
         tab.addEventListener('click', function() {
+            // すべてのタブとコンテンツを非アクティブに
             tabs.forEach(t => t.classList.remove('active'));
             contents.forEach(c => c.classList.remove('active'));
 
+            // クリックされたタブをアクティブに
             tab.classList.add('active');
-            var activeContent = document.querySelector('#content-' + tab.id.split('-')[1]);
+            var activeContentId = 'content-' + tab.id;
+            var activeContent = document.getElementById(activeContentId);
             if (activeContent) {
                 activeContent.classList.add('active');
             }
