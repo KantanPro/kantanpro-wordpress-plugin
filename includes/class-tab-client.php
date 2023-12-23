5c966e3120d416442f0b483a43731f995c6c8254
nonaka <info@kantan-pro.com>
2023-12-23 21:37:01 +0900

    顧客登録OK

--- includes/class-tab-client.php
+++ includes/class-tab-client.php
@@ -1,39 +1,43 @@
 <?php
 
 class KTP_Tab_Client {
-    // コンストラクタ
+    // コンストラクタ: WordPressのフックを設定
     public function __construct() {
+        // 管理メニューに顧客タブを追加するためのフック
         add_action('admin_menu', array($this, 'add_client_menu'));
+        // フォーム送信の処理を行うためのフック
         add_action('admin_post_ktp_add_client', array($this, 'handle_form_submission'));
-        add_action('admin_post_ktp_delete_client', array($this, 'handle_delete_submission'));
     }
 
     // 管理メニューに顧客タブを追加
     public function add_client_menu() {
         add_submenu_page(
-            'ktp-main-menu',
-            '顧客',
-            '顧客',
-            'manage_options',
-            'ktp-tab-client',
-            array($this, 'client_page_content')
+            'ktp-main-menu', // 親メニューのスラッグ
+            '顧客', // ページタイトル
+            '顧客', // メニュータイトル
+            'manage_options', // 必要な権限
+            'ktp-tab-client', // メニュースラッグ
+            array($this, 'client_page_content') // コンテンツを表示するコールバック関数
         );
     }
 
     // 顧客タブのページコンテンツを表示
     public function client_page_content() {
         echo '<h1>顧客管理</h1>';
+        // 顧客データの送信フォームを表示
         $this->display_client_form();
+        // 保存されている顧客データを表示
         $this->display_clients();
     }
 
     // 顧客データの送信フォームを表示
     private function display_client_form() {
+        // フォームのセキュリティ強化のためのノンスを生成
         $nonce = wp_create_nonce('ktp_add_client_nonce');
         ?>
         <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post">
             <input type="hidden" name="action" value="ktp_add_client">
-            <input type="hidden" name "ktp_nonce" value="<?php echo $nonce; ?>">
+            <input type="hidden" name="ktp_nonce" value="<?php echo $nonce; ?>">
             <label for="name">名前：</label>
             <input type="text" id="name" name="name" required><br>
             <label for="email">メールアドレス：</label>
@@ -61,37 +65,12 @@ class KTP_Tab_Client {
 
         // データベースにデータを挿入
         $wpdb->insert(
-            $wpdb->prefix . 'ktp_client',
+            $wpdb->prefix . 'ktp_client', // テーブル名
             array('name' => $name, 'email' => $email),
             array('%s', '%s')
         );
 
-        // 現在のページにリダイレクト
-        wp_redirect($_SERVER['HTTP_REFERER']);
-        exit;
-    }
-
-    // 顧客データの削除処理
-    public function handle_delete_submission() {
-        global $wpdb;
-
-        // ノンスの検証
-        if (!isset($_POST['ktp_nonce']) || !wp_verify_nonce($_POST['ktp_nonce'], 'ktp_delete_client_nonce')) {
-            wp_die(__('Invalid nonce specified', 'ktp'), __('Error', 'ktp'), array(
-                'response'  => 403,
-                'back_link' => 'admin.php?page=ktp-tab-client',
-            ));
-        }
-
-        // データの削除
-        $client_id = intval($_POST['client_id']);
-        $wpdb->delete(
-            $wpdb->prefix . 'ktp_client',
-            array('id' => $client_id),
-            array('%d')
-        );
-
-        // 現在のページにリダイレクト
+        // 前のページにリダイレクト
         wp_redirect($_SERVER['HTTP_REFERER']);
         exit;
     }
@@ -99,28 +78,20 @@ class KTP_Tab_Client {
     // 保存されている顧客データを表示
     private function display_clients() {
         global $wpdb;
-        $table_name = $wpdb->prefix . 'ktp_client';
+        $table_name = $wpdb->prefix . 'ktp_client'; // テーブル名
 
+        // データベースから顧客データを取得
         $clients = $wpdb->get_results("SELECT * FROM $table_name");
 
+        // 顧客データがあればテーブル形式で表示
         if ($clients) {
             echo '<table>';
-            echo '<tr><th>ID</th><th>名前</th><th>メールアドレス</th><th>操作</th></tr>';
+            echo '<tr><th>ID</th><th>名前</th><th>メールアドレス</th></tr>';
             foreach ($clients as $client) {
                 echo '<tr>';
                 echo '<td>' . esc_html($client->id) . '</td>';
                 echo '<td>' . esc_html($client->name) . '</td>';
                 echo '<td>' . esc_html($client->email) . '</td>';
-                echo '<td>';
-                // 削除ボタンの追加
-                $delete_nonce = wp_create_nonce('ktp_delete_client_nonce');
-                echo '<form action="' . esc_url(admin_url('admin-post.php')) . '" method="post">';
-                echo '<input type="hidden" name="action" value="ktp_delete_client">';
-                echo '<input type="hidden" name="ktp_nonce" value="' . $delete_nonce . '">';
-                echo '<input type="hidden" name="client_id" value="' . esc_attr($client->id) . '">';
-                echo '<input type="submit" value="削除">';
-                echo '</form>';
-                echo '</td>';
                 echo '</tr>';
             }
             echo '</table>';
