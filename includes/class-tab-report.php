6e7caae443d0c396626566c4313fd29cbbdf706b
nonaka <info@kantan-pro.com>
2024-01-14 14:39:39 +0900

    タブクリック問題

--- includes/class-tab-report.php
+++ includes/class-tab-report.php
@@ -10,12 +10,30 @@ class Kntan_Report_Class {
     
     function Report_Tab_View( $tab_name ) {
 
+        
         // 表示する内容
         $content = <<<END
         <h3>ここは [$tab_name] です。</h3>
-        売上などのレポートを表示できます。
         END;
-        return $content;
+        // return $content;
+
+        // 有効化を確認
+        $activation_key = get_site_option( 'ktp_activation_key' );
+        if ( empty( $activation_key ) ) {
+            $content .= <<<END
+            カンタンProWPは有効化されていません。<br />
+            WordPressの管理画面で、設定→カンタンProWPで有効化キーを設定してください。<br />
+            売上などのレポートを表示できます。
+            END;
+            return $content;
+        } else {
+            $content .= <<<END
+            カンタンProWPの有効化ありがとうございます！<br />
+            売上などのレポートを表示できます。<br />
+            今、開発中なので、しばらくお待ちください。
+            END;
+            return $content;
+        }
     }
 }
 
