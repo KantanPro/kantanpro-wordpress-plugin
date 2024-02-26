8b6d13e3654af653933dcf3fa7d908c03eb9acad
nonaka <info@kantan-pro.com>
2024-02-26 23:15:51 +0900

    テンプレートファイルのアップロード機能を修正し、データベースからテンプレートを取得するように変更

--- includes/class-print.php
+++ includes/class-print.php
@@ -9,29 +9,12 @@ class Print_Class {
 
     // Generate HTML
     public function generateHTML() {
-        $template = file_get_contents(__DIR__ . '/../template/template.txt');
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'ktp_setting'; // テーブル名を適切に設定してください
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $allowedExtensions = ['txt'];
-            $uploadDir = __DIR__ . '/../template/';
-            $uploadFile = $uploadDir . basename($_FILES['templateFile']['name']);
-            $fileExtension = strtolower(pathinfo($uploadFile, PATHINFO_EXTENSION));
-
-            if (in_array($fileExtension, $allowedExtensions) && move_uploaded_file($_FILES['templateFile']['tmp_name'], $uploadFile)) {
-                $newUploadFile = $uploadDir . 'template.txt';
-                rename($uploadFile, $newUploadFile);
-                $template = file_get_contents($newUploadFile);
-            //     echo '<script>alert("ファイルが有効で、アップロードされました。");</script>';
-            // } else {
-            //     echo '<script>alert("アップロードされたファイルは無効です。");</script>';
-            }
-        }
-
-        // // Display the file upload form
-        // <form method="POST" enctype="multipart/form-data">
-        //     <input type="file" name="templateFile" accept=".txt">
-        //     <button type="submit">Upload</button>
-        // </form>
+        // データベースからテンプレートを取得
+        $template_row = $wpdb->get_row("SELECT * FROM $table_name WHERE id = 1");
+        $template = $template_row->template_content;
 
         $replacements = array(
             '_%postal_code%_' => $this->data['postal_code'],
@@ -46,19 +29,6 @@ class Print_Class {
 
         return $html;
     }
-
 }
-// $data = array(
-//     'customer' => 'John Doe',
-//     'amount' => 1000
-// );
-
-// $printEstimate = new Print_Class($data);
-// $html = $printEstimate->generateHTML();
-// echo $html;
-
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
 
 ?>
