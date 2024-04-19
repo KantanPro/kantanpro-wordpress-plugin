dab656e6a0b83f0f0a8782bb8b3d2c75ce033db3
nonaka@MacBook <kantanpro22@gmail.com>
2024-04-19 14:18:03 +0900

    テーブルの作成と更新処理を追加しました

--- includes/class-tab-client.php
+++ includes/class-tab-client.php
@@ -28,6 +28,37 @@ class Kntan_Client_Class {
         $my_table_version = '1.0.1';
         $table_name = $wpdb->prefix . 'ktp_' . $tab_name;
         $charset_collate = $wpdb->get_charset_collate();
+
+        // テーブルが存在しない場合は作成
+        if($wpdb->get_var("SHOW TABLES LIKE '{$table_name}'") != $table_name) {
+            $sql = "CREATE TABLE {$table_name} (
+                id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
+                time BIGINT(11) DEFAULT '0' NOT NULL,
+                name TINYTEXT NOT NULL,
+                url VARCHAR(55) NOT NULL,
+                company_name VARCHAR(100) NOT NULL DEFAULT 'いつもの業者',
+                email VARCHAR(100) NOT NULL,
+                phone VARCHAR(20) NOT NULL,
+                postal_code VARCHAR(10) NOT NULL,
+                prefecture TINYTEXT NOT NULL,
+                city TINYTEXT NOT NULL,
+                address TEXT NOT NULL,
+                building TINYTEXT NOT NULL,
+                closing_day TINYTEXT NOT NULL,
+                payment_month TINYTEXT NOT NULL,
+                payment_day TINYTEXT NOT NULL,
+                payment_method TINYTEXT NOT NULL,
+                tax_category VARCHAR(100) NOT NULL DEFAULT '税込',
+                memo TEXT NOT NULL,
+                search_field TEXT NOT NULL,
+                frequency INT NOT NULL DEFAULT 0,
+                category VARCHAR(100) NOT NULL DEFAULT '一般',
+                UNIQUE KEY id (id)
+            ) {$charset_collate};";
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            dbDelta($sql);
+            add_option("{$table_name}_version", $my_table_version);
+        }
     
         $columns = [
             "id MEDIUMINT(9) NOT NULL AUTO_INCREMENT",
@@ -78,6 +109,65 @@ class Kntan_Client_Class {
 
         global $wpdb;
         $table_name = $wpdb->prefix . 'ktp_' . $tab_name;
+
+        // POSTデーター受信
+        $data_id = $_POST['data_id'];
+        // その他のPOSTデータを受信...
+
+        // データIDが指定されているか確認
+        if (!empty($data_id)) {
+        // データが存在するか確認
+        $exists = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE id = %d", $data_id));
+        if ($exists) {
+        // 既存のデータを更新
+        $wpdb->update(
+            $table_name,
+            array( /* 更新するデータの配列 */ ),
+            array('id' => $data_id) // 条件
+        );
+        } else {
+        // 新しいデータを追加
+        $wpdb->insert(
+            $table_name,
+            array( /* 追加するデータの配列 */ )
+        );
+        }
+        } else {
+        // $data_idが不適切な場合のエラーハンドリング
+        }
+
+        // データが0の場合、デフォルトデータを1つ作成する
+        $data_count = $wpdb->get_var("SELECT COUNT(*) FROM $table_name");
+        if($data_count == 0) {
+            $default_data = [
+                'time' => current_time('mysql'),
+                'name' => 'デフォルト名',
+                'url' => '',
+                'company_name' => 'いつもの業者',
+                'representative_name' => '',
+                'email' => '',
+                'phone' => '',
+                'postal_code' => '',
+                'prefecture' => '',
+                'city' => '',
+                'address' => '',
+                'building' => '',
+                'closing_day' => '',
+                'payment_month' => '',
+                'payment_day' => '',
+                'payment_method' => '',
+                'tax_category' => '税込',
+                'memo' => '',
+                'search_field' => '',
+                'frequency' => 0,
+                'category' => '一般'
+            ];
+            $wpdb->insert($table_name, $default_data);
+            $data_id = $wpdb->insert_id;
+            // 作成したデータIDをクッキーに保存する
+            $cookie_name = 'ktp_' . $tab_name . '_id';
+            setcookie($cookie_name, $data_id, time() + (86400 * 30), "/"); // 30日間有効
+        }
 
         // テーブル名にロックをかける
         $wpdb->query("LOCK TABLES {$table_name} WRITE;");
@@ -382,11 +472,12 @@ $cookie_name = 'ktp_' . $tab_name . '_id';
                 // ロックを解除する
                 $wpdb->query("UNLOCK TABLES;");
 
-                // 追加後に更新モードにする
-                // リダイレクト
+                // 追加後に更新モードにしてリダイレクトしIDをクッキーに保存
+                $new_data_id = $wpdb->insert_id;
                 $action = 'update';
-                $data_id = $wpdb->insert_id;
-                $url = '?tab_name='. $tab_name . '&data_id=' . $data_id . '&query_post=' . $action;
+                $url = '?tab_name='. $tab_name . '&data_id=' . $new_data_id . '&query_post=' . $action;
+                $cookie_name = 'ktp_' . $tab_name . '_id'; // クッキー名を設定
+                setcookie($cookie_name, $new_data_id, time() + (86400 * 30), "/"); // クッキーを保存
                 header("Location: {$url}");
                 exit;
             }
@@ -456,82 +547,7 @@ $cookie_name = 'ktp_' . $tab_name . '_id';
             }
         }
         
-        // 
-        // 商品画像処理
-        // 
-
-        // 画像をアップロード
-        elseif ($query_post == 'upload_image') {
-
-
-            // 画像URLを取得
-            $image_processor = new Image_Processor();
-            $default_image_url = plugin_dir_url(''). 'kantan-pro-wp/images/default/no-image-icon.jpg';
-            $image_url = $image_processor->handle_image($tab_name, $data_id, $default_image_url);
-
-            // echo '$tab_name：'.$tab_name.'<br>$data_id：'.$data_id.'<br>$default_image_url：'.$default_image_url.'<br>$image_url：'.$image_url;
-            // exit;
-
-            $wpdb->update(
-                $table_name,
-                array(
-                    'image_url' => $image_url
-                ),
-                array(
-                    'id' => $data_id
-                ),
-                array(
-                    '%s'
-                ),
-                array(
-                    '%d'
-                )
-            );
-
-            // echo $image_url;
-            // exit;
-
-            // リダイレクト
-            $url = '?tab_name='. $tab_name . '&data_id=' . $data_id;
-            header("Location: {$url}");
-            exit;
-        }
-
-        // 画像削除：デフォルト画像に戻す
-        elseif ($query_post == 'delete_image') {
-
-            // デフォルト画像のURLを設定
-            $default_image_url = plugin_dir_url(''). 'kantan-pro-wp/images/default/no-image-icon.jpg';
-
-            $wpdb->update(
-                $table_name,
-                array(
-                    'image_url' => $default_image_url
-                ),
-                array(
-                    'id' => $data_id
-                ),
-                array(
-                    '%s'
-                ),
-                array(
-                    '%d'
-                )
-            );
-
-            // リダイレクト
-            $url = '?tab_name='. $tab_name . '&data_id=' . $data_id;
-            header("Location: {$url}");
-            exit;
-        }
-
-        // どの処理にも当てはまらない場合はロック解除
-        else {
-            // ロックを解除する
-            $wpdb->query("UNLOCK TABLES;");
-        }
-
-
+        
     }
 
     
@@ -665,6 +681,7 @@ $cookie_name = 'ktp_' . $tab_name . '_id';
              <a href="?tab_name=$name&page_start=$last_start&page_stage=2&flg=$flg">>|</a>
             END;
         }
+
                         
         $results_f .= "</div></div>";
 
@@ -818,7 +835,7 @@ $cookie_name = 'ktp_' . $tab_name . '_id';
                 if( $action === 'istmode'){
                     // 追加実行ボタン
                     $action = 'insert';
-                    $data_id = $data_id + 1;
+                    // $data_id = $data_id + 1;
                     $data_forms .= <<<END
                     <form method='post' action=''>
                     <input type='hidden' name='query_post' value='$action'>
