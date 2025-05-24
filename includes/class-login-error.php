fce6324402507dbe6eb579894416e400ecfafe69
aiojiipg <kantanpro22@gmail.com>
2025-05-25 06:05:04 +0900

    ログインエラーメッセージの表示内容を「カンタンPro」から「KTPWP」に変更

--- includes/class-login-error.php
+++ includes/class-login-error.php
@@ -15,14 +15,12 @@ class Kantan_Login_Error{
     function Error_View() {
 
         // ログインのリンク
-        $login_link = wp_login_url(); 
-
-        // 表示する内容
+        $login_link = wp_login_url();        // 表示する内容
         $content = <<<END
-        <h3>カンタンProを利用するにはログインしてください。</h3>
+        <h3>KTPWPを利用するにはログインしてください。</h3>
         <!--ログイン-->
         <p><font size="4"><a href="$login_link">ログイン</a></font>　
-        <font size="4"><a href="/welcome-to-kantanprowp/">ホームへ</a></font></p>
+        <font size="4"><a href="/welcome-to-ktpwp/">ホームへ</a></font></p>
         END;
         return $content;
     }
